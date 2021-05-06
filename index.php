57c9ec23698a073df86bb42669bc23a179f58a7b
simohamedhdafa <simohamed.hdafa@gmail.com>
2021-05-06 10:16:16 +0000

    BODY CARDS INSERTED

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
     //afficher($_GET);
     $class = isset($_GET['split']) ? $_GET['split'] : "train";
+
+    if(!in_array($class, array("train", "val", "test"))){
+        die('Error!');
+    }
     // Read JSON file
     $json = file_get_contents('database/dataset_flickr8k.json');
 
@@ -55,30 +59,57 @@
     <title>image captioning</title>
     
     <?php foreach($css_list_paths as $cssf){ ?>
-    <link rel="stylesheet" <?php echo "href=\"".substr($cssf, strlen("C:\wamp64\www\DS3PHPYM22021simohamedhdafa\\"))."\""; ?> >
+    <link rel="stylesheet" <?php echo "href=\"".substr($cssf, strlen("C:\wamp64\www\DS3PHPYM22021simohamedhdafa-2\\"))."\""; ?> >
     <?php } ?>
 
     <?php foreach($js_list_paths as $jsf){ ?>
-    <script type="text/javascript" <?php echo "src=\"".substr($jsf, strlen("C:\wamp64\www\DS3PHPYM22021simohamedhdafa\\"))."\""; ?>></script>
+    <script type="text/javascript" <?php echo "src=\"".substr($jsf, strlen("C:\wamp64\www\DS3PHPYM22021simohamedhdafa-2\\"))."\""; ?>></script>
     <?php } ?>
 </head>
 <body>
-    <div class="container-md">
-        <h1>Hellow yncrea!</h1>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="<?php echo $_SERVER['PHP_SELF'] . "?split=train" ?>">Training</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo $_SERVER['PHP_SELF'] . "?split=val" ?>">Validation</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo $_SERVER['PHP_SELF'] . "?split=test" ?>">Testing</a>
-            </li>
-        </ul>
 
-        <?php afficher($content); ?>
+    <div class="card">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="true" href="<?php echo $_SERVER['PHP_SELF'] . "?split=train" ?>">Training</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $_SERVER['PHP_SELF'] . "?split=val" ?>">Validation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $_SERVER['PHP_SELF'] . "?split=test" ?>" tabindex="-1" aria-disabled="true">Testing</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+        <?php 
+            
+            for($i=0; $i<100; $i++){
+                //afficher($content[$i]);
+                $k = key($content[$i]);
+                $v = $content[$i][$k];
+        ?>
 
+        <div class="card mb-3" style="max-width: 840px;">
+            <div class="row g-0">
+                <div class="col-md-3">
+                    <img src="<?php echo "\database\Images\\" . $k; ?>" class="img-thumbnail" alt="...">   
+                </div>
+                <div class="col-md-9">
+                    <div class="card-body">
+                        <p class="card-text"><?php echo $v[0]; ?></p>
+                        <p class="card-text"><?php echo $v[1]; ?></p>
+                        <p class="card-text"><?php echo $v[2]; ?></p>
+                        <p class="card-text"><?php echo $v[3]; ?></p>
+                        <p class="card-text"><?php echo $v[4]; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>    
+            
+        <?php } ?>
+        </div>
     </div>
 </body>
 </html>
